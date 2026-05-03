22c23d8c2569bd36b6d20a2a616c06538c37a65b
Luca <jeanlucavalencia08@gmail.com>
2026-05-03 01:51:04 -0700

    Finish token logic and store args commands in array. Set up fork

--- main.c
+++ main.c
@@ -1,50 +1,68 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-//main shell loop:
-int main() {
-    while(1){
-        //print prompt
+// main shell loop:
+int main()
+{
+    while (1)
+    {
+        // print prompt
         printf("my_shell> ");
-        
-        
-        
-        //read user input
+
+        // read user input
         char input[100];
-        
-        //read user input
-       if (fgets(input, sizeof(input), stdin) == NULL){
-        break;
-       }
 
-       //remove newline character:
-       input[strcspn(input, "\n")] = 0;
+        // read user input
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            break;
+        }
 
-       // if user just pressed Enter, skip and show prompt again
-       if(strlen(input) == 0)
+        // remove newline character:
+        input[strcspn(input, "\n")] = 0;
+
+        // if user just pressed Enter, skip and show prompt again
+        if (strlen(input) == 0)
             continue;
 
-       //process the exit command
+        // process the exit command
 
-       if(strcmp(input, "exit") == 0)
+        if (strcmp(input, "exit") == 0)
             break;
 
-       
-       //tokenize the input
+        // tokenize the input
 
-       
-       char delimiter[] = " ";
-       
-       char *token = strtok(input, delimiter);
+        char delimiter[] = " ";
 
-    while(token != NULL){
+        char *token = strtok(input, delimiter);
+
+        // array of string pointers to store command and arguments
+        char *args[100];
+
+        int i = 0;
+        while (token != NULL)
+        {
+            // print the token
             printf("Token: %s\n", token);
+            // Store the token in the args array
+            args[i] = token;
+
+            i++;
             token = strtok(NULL, delimiter);
         }
 
+        // Null-terminate the args array
+        args[i] = NULL;
 
+        // for a new process
+        pid_t p = fork();
 
-        
-
+        if (p == 0)
+        {
+            // child process
+            printf("Child process");
+        }
     }
 }
